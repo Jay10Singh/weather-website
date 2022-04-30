fc349699be4b20a468674e13bfe805d0c7f6341e
Jay Singh Yadav <jaysingh333yadav@gmail.com>
2022-04-30 10:06:46 +0530

    Adding Humidity in forecast

--- src/utils/foreCast.js
+++ src/utils/foreCast.js
@@ -4,13 +4,13 @@ const foreCast = (longitude, latitude, callback) => {
 
     const url = 'http://api.weatherstack.com/current?access_key=e2cb952bd10244fb26fc9579eb637d0f&query=' + longitude + ',' + latitude + '&units=f';
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url: url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather stack api', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, `${response.body.current.weather_descriptions[0]}.It is currently ${response.body.current.temperature} degree out. But it feels like ${response.body.current.feelslike} degrees out `);
+            callback(undefined, `${body.current.weather_descriptions[0]}.It is currently ${body.current.temperature} degree out. But it feels like ${body.current.feelslike} degrees out. The Humidity is ${body.current.humidity} %`);
         }
     })
 
